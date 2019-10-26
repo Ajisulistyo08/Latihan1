1c3054993e32cfe67b0fc4ff14ed1763fc51f933
Ajisulistyo08 <56190945+Ajisulistyo08@users.noreply.github.com>
2019-10-26 15:31:50 +0700

    Add files via upload

--- Tugas 1.py
+++ Tugas 1.py
@@ -8,4 +8,4 @@ umur = "22"
 telf = "087834443643"
 alamat = "Karawang Timur"
 
-print("Assalamualaikum, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,", My NPM is",npm,", I was born in",tempatlahir,",and I am",umur,",years old. I am very glad if you want to invite my house in",alamat,", So, don't forget to call me before with the number",telf," \n Thank You")
+print("Assalamualaikum, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,", My NPM is",npm,",\n I was born in",tempatlahir,",and I am",umur,",years old. I am very glad if you want to invite my house in",alamat,",\n So, don't forget to call me before with the number",telf," \n Thank You")
