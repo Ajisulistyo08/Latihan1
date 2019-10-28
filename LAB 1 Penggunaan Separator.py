5d506ff5804ad9c4b34325d0bb094180ef01fd6a
Ajisulistyo08 <56190945+Ajisulistyo08@users.noreply.github.com>
2019-10-28 22:43:47 +0700

    Add files via upload

---
+++ LAB 1 Penggunaan Separator.py
@@ -0,0 +1,10 @@
+# penggunaan separator
+w, x, y, z, = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
+
+
+#Penjelasan : program untuk menambahkan simbol dari suatu bilangan seperti tanda koma(','), titik dua(':'), strip('---')
