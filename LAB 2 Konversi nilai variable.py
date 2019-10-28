81d972b3f47ea205bba5ab3be83840d9727ed0ee
Ajisulistyo08 <56190945+Ajisulistyo08@users.noreply.github.com>
2019-10-28 22:49:26 +0700

    Add files via upload

---
+++ LAB 2 Konversi nilai variable.py
@@ -0,0 +1,13 @@
+a = input("Masukkan nilai a:")
+b = input("Masukkan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+#konversi nilai variable
+a = int(a)
+b = int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+
+#Penjelasan: program untuk mencari hasil penggabungan, penjumlahan, pembagian
