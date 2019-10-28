4dfa418443541d65ba80091f38f86087e638d06f
Ajisulistyo08 <56190945+Ajisulistyo08@users.noreply.github.com>
2019-10-28 22:41:17 +0700

    Add files via upload

---
+++ LAB 1 Penggunaan END.py
@@ -0,0 +1,10 @@
+# penggunaan end
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('Z')
+
+#penjelasan: Suatu program untuk membuat suatu kalimat berbaris berbentuk horizontal dan vertikal
